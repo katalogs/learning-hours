5203467708406e337ae1b683d5db4b981719a132
Louis Michon <louis.michon@cdbdx.biz>
2022-07-04 14:59:59 +0200

    wip

--- functional-programming/1-pure-functions/Rental/Rental.Tests/RentalCalculatorShould.cs
+++ functional-programming/1-pure-functions/Rental/Rental.Tests/RentalCalculatorShould.cs
@@ -62,5 +62,42 @@ namespace Rental.Tests
                     "Total amount | 3037.2400000000002"
                 );
         }
+
+        [Fact]
+        public void CalculateRentals()
+        {
+            var calculator = new RentalCalculator(SomeRentals());
+            double totalAmount = calculator.Calculate();
+
+            totalAmount
+                .Should()
+                .BeApproximately(3037.24, 0.001);
+
+        }
+
+        [Fact]
+        public void FormatStatement()
+        {
+            var calculator = new RentalCalculator(SomeRentals());
+            var statement = calculator.Calculate();
+
+            calculator.IsCalculated
+                .Should()
+                .BeTrue();
+
+            calculator.Amount
+                .Should()
+                .BeApproximately(3037.24, 0.001);
+
+            statement.Should()
+                .Be(
+                    "09-10-2020 : Le Refuge des Loups (LA BRESSE) | 1089.9" + NewLine +
+                    "12-10-2020 : Au pied de la Tour (NOUILLORC) | 1276.45" + NewLine +
+                    "24-10-2020 : Le moulin du bonheur (GLANDAGE) | 670.89" + NewLine +
+                    "Total amount | 3037.2400000000002"
+                );
+        }
+
+
     }
 }
