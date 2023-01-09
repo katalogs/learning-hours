1f8a80cb51a2028c53c3556d318006c59d009e23
Anas Hoummadi <anas.hoummadi@cdbdx.biz>
2023-01-09 11:58:47 +0100

    Set rule Command handler should only return ints

--- archunit/archunit-kata/ArchUnit-kata-tests/MethodReturnTypes.cs
+++ archunit/archunit-kata/ArchUnit-kata-tests/MethodReturnTypes.cs
@@ -1,4 +1,15 @@
-﻿using Xunit;
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent.Syntax.Elements.Members.MethodMembers;
+
+using ArchUnit.Kata.Layered.Controllers;
+using ArchUnit.Kata.Layered.Models;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Fluent.Syntax.Elements.Types;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+using Xunit;
 using static ArchUnit.Kata.Tests.ArchUnitExtensions;
 
 namespace ArchUnit.Kata.Tests
@@ -6,8 +17,10 @@ namespace ArchUnit.Kata.Tests
     public class MethodReturnTypes
     {
         [Fact]
-        public void CommandHandlersShouldOnlyReturnInt() =>
-            EmptyRule("Command handler should only return ints")
+        public void CommandHandlersShouldOnlyReturnInt() => MethodMembers()
+            .That().AreDeclaredIn(typeof(SuperHeroController))
+            .Should().HaveReturnType(typeof(int))
+            .Because("Command handler should only return ints")
                 .Check();
 
         [Fact]
