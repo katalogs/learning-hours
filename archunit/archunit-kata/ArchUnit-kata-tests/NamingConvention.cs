c92eed7e196c03264f7c564e7299aff24338c7af
Anas Esseidi <anas.esseidi@cdbdx.biz>
2023-01-23 11:23:12 +0100

    Add test on Services suffixe

--- archunit/archunit-kata/ArchUnit-kata-tests/NamingConvention.cs
+++ archunit/archunit-kata/ArchUnit-kata-tests/NamingConvention.cs
@@ -1,5 +1,8 @@
-﻿using Xunit;
-using static ArchUnit.Kata.Tests.ArchUnitExtensions;
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+
 
 namespace ArchUnit.Kata.Tests
 {
@@ -7,12 +10,14 @@ namespace ArchUnit.Kata.Tests
     {
         [Fact]
         public void ServicesShouldBeSuffixedByService() =>
-            EmptyRule("Each Service should be suffixed by Service")
-                .Check();
+            Classes().That().ResideInNamespace("ArchUnit.Kata.Layered.Services").Should()
+            .HaveNameEndingWith("Service")
+            .Because("Each Service should be suffixed by Service")
+            .Check();
 
-        [Fact]
-        public void CommandHandlersShouldBeSuffixedByCommandHandler() =>
-            EmptyRule("Command handlers should be suffixed by CommandHandler")
-                .Check();
+        //[Fact]
+        //public void CommandHandlersShouldBeSuffixedByCommandHandler() =>
+        //    //EmptyRule("Command handlers should be suffixed by CommandHandler")
+        //        .Check();
     }
 }
