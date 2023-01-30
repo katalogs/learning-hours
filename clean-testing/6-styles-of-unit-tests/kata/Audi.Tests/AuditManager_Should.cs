c734af252ff386615066dfd334e1a1b526ab802c
Lucas Consejo <lucas.consejo@cdbdx.biz>
2023-01-30 16:43:38 +0100

    Add InMemoryFileSystem

--- clean-testing/6-styles-of-unit-tests/kata/Audi.Tests/AuditManager_Should.cs
+++ clean-testing/6-styles-of-unit-tests/kata/Audi.Tests/AuditManager_Should.cs
@@ -43,4 +43,36 @@ public class AuditManager_Should
             Path.Combine(DirectoryName, "audit_3.txt"),
             "Alice;2019-04-06 18:00:00"));
     }
+
+    [Fact]
+    public void it_should_add_record()
+    {
+        // Arrange
+        var fileSystemMock = new Mock<IFileSystem>();
+        fileSystemMock
+            .Setup(x => x.GetFiles(DirectoryName))
+            .Returns(new string[]
+            {
+                Path.Combine(DirectoryName, "audit_1.txt"),
+                Path.Combine(DirectoryName, "audit_2.txt")
+            });
+
+        fileSystemMock
+            .Setup(x => x.ReadAllLines(Path.Combine(DirectoryName, "audit_2.txt")))
+            .Returns(new List<string>
+            {
+                "Peter;2019-04-06 16:30:00",
+                "Jane;2019-04-06 16:40:00",
+                "Jack;2019-04-06 17:00:00"
+            });
+        var sut = new AuditManager(3, DirectoryName, fileSystemMock.Object);
+
+        // Act
+        sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
+
+        // Assert
+        fileSystemMock.Verify(x => x.WriteAllText(
+            Path.Combine(DirectoryName, "audit_3.txt"),
+            "Alice;2019-04-06 18:00:00"));
+    }
 }
---
+++ clean-testing/6-styles-of-unit-tests/kata/Audi.Tests/InMemoryFileSystem.cs
@@ -0,0 +1,32 @@
+﻿using Audit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audi.Tests
+{
+    public class InMemoryFileSystem : IFileSystem 
+    {
+        public void AddFiles()
+        {
+
+        }
+
+        public string[] GetFiles(string directoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ReadAllLines(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllText(string filePath, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
